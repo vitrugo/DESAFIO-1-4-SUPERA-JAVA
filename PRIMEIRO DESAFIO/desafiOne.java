b6fa9be745877d07871ac0bc1b29ca01686a36d6
vitrugo <51348273+vitrugo@users.noreply.github.com>
2022-05-06 13:49:54 -0300

    Final Primeiro Desafio

--- PRIMEIRO DESAFIO/desafiOne.java
+++ PRIMEIRO DESAFIO/desafiOne.java
@@ -5,35 +5,40 @@ import java.util.Scanner;
 
 public class desafiOne {
 
-	public static void main (String[] args) {
-		Scanner sc = new Scanner(System.in);		
-		
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
 		List<Integer> lista = new ArrayList<>();
-		List<Integer> par 	= new ArrayList<>();
+		List<Integer> par = new ArrayList<>();
 		List<Integer> impar = new ArrayList<>();
-		
+
+		// System.out.println("Insira a quantidade de numeros que deseja colocar em
+		// sequencia: ");
 		int n = sc.nextInt();
-		
-		for (int i=0 ; i<n ; i++)
+
+		for (int i = 0; i < n; i++)
 			lista.add(sc.nextInt());
-				
-		for (Integer x: lista){
-			if (x%2==0)
+
+		for (Integer x : lista) {
+			if (x % 2 == 0)
 				par.add(x);
 			else
 				impar.add(x);
 		}
-		
+
 		Collections.sort(par);
 		Collections.sort(impar);
 		Collections.reverse(impar);
-		
-		for (Integer y: par)
-			System.out.println(y);
-		for (Integer z: impar)
-			System.out.println(z);
-		
+
+		// System.out.println("Numeros Pares: ");
+		for (Integer p : par) {
+			System.out.println(p);
+		}
+		// System.out.println("Numeros impares: ");
+		for (Integer i : impar) {
+			System.out.println(i);
+		}
 		sc.close();
 	}
-	
+
 }
